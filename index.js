fa2c17ae121afee7ae987245d75dccf2488751fd
Drumil Akhenia <160459609+drumil1602@users.noreply.github.com>
2024-02-29 22:58:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+// const express = require("express"); // CJS
+import express from "express"; // ESM
+import fs from "node:fs";
+import { nanoid } from "nanoid";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+
+const __filepath = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filepath)
+// console.log(__dirname)
+const app = express();
+
+const isURLValid = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  console.log(__dirname);
+  console.log(path.join(__dirname,"index.html"));
+  // res.send("hello")
+  res.sendFile(path.join(__dirname,"index.html"));
+});
+
+app.post("/shorten", (req, res) => {
+  const longUrl = req.body.longUrl;
+  const shortUrl = nanoid(10);
+  const isValid = isURLValid(longUrl);
+  if (!isValid) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid URL",
+    });
+  }
+  const fileResponse = fs.readFileSync("urlmap.json");
+  const fileData = JSON.parse(fileResponse.toString());
+
+  fileData[shortUrl] = longUrl;
+
+  fs.writeFileSync("urlmap.json", JSON.stringify(fileData));
+
+  res.json({
+    succes: true,
+    url: `http://localhost:5000/${shortUrl}`,
+  });
+});
+
+app.get("/:shortUrl", (req, res) => {
+  const fileResponse = fs.readFileSync("urlmap.json");
+  const fileData = JSON.parse(fileResponse.toString());
+  const longUrl = fileData[req.params.shortUrl];
+  // If longurl doesn't exists show 404 with json
+  res.redirect(longUrl);
+});
+
+app.listen(5000, () => console.log("App is up and running at port 5000"));
